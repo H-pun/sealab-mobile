28d0f940fb36d4e11d0925024d058d9bf129c329
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-06-11 11:18:38 +0700

    feat: datepicker with config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { PaperProvider, DefaultTheme } from 'react-native-paper';
+import { en, registerTranslation } from 'react-native-paper-dates'
 
 import Router from './src/router/index.js';
 
@@ -8,13 +9,14 @@ const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: '#24A9E1', // Ganti dengan warna primer yang diinginkan
+    primary: '#24A9E1',
     primaryContainer: 'black',
     secondaryContainer: "#C9EBF5",
   },
 };
 
 function App(): JSX.Element {
+  registerTranslation('en', en)
   return (
     <PaperProvider theme={theme}>
       <NavigationContainer>
