6c7d832cb782f902f51ece8f99de982bd501da6d
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2023-06-11 11:20:34 +0700

    feat: score modal and input

--- src/pages/ScoreInput/index.js
+++ src/pages/ScoreInput/index.js
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios from '../../utils/api';
+import moment from 'moment-timezone';
 import React, { useState, useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import env from '../../../env';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { DatePickerInput } from 'react-native-paper-dates';
+
 
 import {
+    ActivityIndicator,
     Button,
-    IconButton,
-    List,
-    Chip,
     Divider,
     Badge,
-    FAB,
-    SegmentedButtons,
-    RadioButton,
+    Text,
+    TextInput,
     DataTable,
+    Modal,
+    Dialog,
+    Portal,
     Card
 } from 'react-native-paper';
 
@@ -22,37 +22,71 @@ import {
     ScrollView,
     StyleSheet,
     View,
-    Alert
+    Alert,
 } from 'react-native';
 
 const InputScore = ({ route, navigation }) => {
-    // const [day, setDay] = React.useState('3');
-    // const [shift, setShift] = React.useState('3');
-    const [data, setData] = React.useState([]);
+    const [module, setModule] = useState('');
+    const [date, setDate] = useState(new Date());
     const [isLoading, setIsLoading] = useState(false);
+    const [btnLoading, setBtnLoading] = useState(false);
+    const [modalVisible, setModalVisible] = React.useState(false);
+    const [dialogVisible, setDialogVisible] = React.useState(false);
+    const [scores, setScores] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    const request = route.params;
+    const { day, shift, group } = route.params;
+    const scoreField = ['tp', 'ta', 'd', 'i1', 'i2'];
+
+    const updateScore = (propertyName, value) => {
+        setScores(prevScores => {
+            const updatedScores = [...prevScores];
+            updatedScores[currentIndex][propertyName] = value;
+            updatedScores[currentIndex].status = updatedScores[currentIndex].d != 0;
+            return updatedScores;
+        });
+    };
+
+    const handleInputScore = async () => {
+        setBtnLoading(true);
+        var dateNow = moment.utc(date).tz('Asia/Jakarta').format('YYYY-MM-DD');
+        console.log({ day, shift, group, module, date: dateNow, scores });
+
+        await axios.post('/api/Seelabs/score/input', { day, shift, group, module, date: dateNow, scores })
+            .then(({ data }) => {
+                console.log(data);
+                setDialogVisible(true);
+            })
+            .catch(({ response }) => {
+                console.log(response.data)
+                Alert.alert("Error", response.data.message)
+            });
+        setBtnLoading(false);
+    };
 
     const handleGetSheet = async () => {
         setIsLoading(true);
-        setData(null);
-        try {
-            axios.interceptors.request.use(async (config) => {
-                const token = await AsyncStorage.getItem('app_token');
-                if (token)
-                    config.headers.Authorization = `Bearer ${token}`;
-                return config;
-            });
-
-            await axios.post(env.API_URL + '/api/Seelabs/score/list-group', request)
-                .then(async ({ data }) => {
-                    setData(data.data);
-                    console.log(data.data);
+        await axios.post('/api/Seelabs/score/list-group', { day, shift, group })
+            .then(({ data }) => {
+                var temp = [];
+                data.data.forEach((item) => {
+                    temp.push({
+                        name: item.name,
+                        uid: item.uid,
+                        status: false,
+                        tp: 0,
+                        ta: 0,
+                        i1: 0,
+                        i2: 0,
+                        d: 0
+                    })
                 });
-        } catch (error) {
-            console.log(error)
-            Alert.alert('Invalid Token', 'Session Expired');
-        }
+                setScores(temp);
+            })
+            .catch(({ response }) => {
+                console.log(response.data)
+                Alert.alert("Error", response.data.message)
+            });
         setIsLoading(false)
     };
 
@@ -60,18 +94,47 @@ const InputScore = ({ route, navigation }) => {
         const fetchData = async () => {
             await handleGetSheet();
         }
-
         fetchData();
     }, [])
 
     return (
         <ScrollView style={styles.container}>
-            {data && data.map((item, index) => (
+            <Portal>
+                <Dialog visible={dialogVisible} onDismiss={() => { setDialogVisible(false) }}>
+                    <Dialog.Title>Alert</Dialog.Title>
+                    <Dialog.Content>
+                        <Text variant="bodyMedium">Input Success</Text>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <Button onPress={() => { setDialogVisible(false) }}>Done</Button>
+                    </Dialog.Actions>
+                </Dialog>
+                <Modal visible={modalVisible} onDismiss={() => setModalVisible(false)} contentContainerStyle={styles.modalContainer}>
+                    <View style={styles.formContainer} enabled>
+                        <Text variant='titleLarge' numberOfLines={1} style={styles.title}>{scores[currentIndex]?.name}</Text>
+                        <Text variant='bodyMedium' style={styles.subTitle}>UID: {scores[currentIndex]?.uid}</Text>
+                        {
+                            scoreField.map((item, index) => (
+                                <TextInput
+                                    key={index}
+                                    style={{ marginBottom: 10 }}
+                                    mode='outlined'
+                                    label={item}
+                                    onChangeText={value => updateScore(item, value)}
+                                    inputMode='numeric'
+                                    value={scores[currentIndex]?.[item] != 0 ? scores[currentIndex]?.[item].toString() : ''}
+                                />
+                            ))
+                        }
+                    </View>
+                </Modal>
+            </Portal>
+            {scores && scores.map((item, index) => (
                 <Card style={{ margin: 20 }} mode='outlined' key={item.uid}>
                     <Card.Title
                         title={item.name}
                         subtitle={`UID: ${item.uid}`}
-                        right={() => <Badge size={25} style={{ marginRight: 15, backgroundColor: 'green' }}> Hadir </Badge>}
+                        right={() => <Badge size={25} style={{ marginRight: 15, backgroundColor: item.status ? 'green' : 'maroon' }}> {item.status ? 'Present' : 'Absent'} </Badge>}
                     />
                     <Divider bold />
                     <Card.Content style={{ flexDirection: 'row' }}>
@@ -85,21 +148,61 @@ const InputScore = ({ route, navigation }) => {
                             </DataTable.Header>
 
                             <DataTable.Row>
-                                <DataTable.Cell style={styles.title}>0</DataTable.Cell>
-                                <DataTable.Cell style={styles.title}>0</DataTable.Cell>
-                                <DataTable.Cell style={styles.title}>0</DataTable.Cell>
-                                <DataTable.Cell style={styles.title}>0</DataTable.Cell>
-                                <DataTable.Cell style={styles.title}>0</DataTable.Cell>
+                                <DataTable.Cell style={styles.title}>{item.tp}</DataTable.Cell>
+                                <DataTable.Cell style={styles.title}>{item.ta}</DataTable.Cell>
+                                <DataTable.Cell style={styles.title}>{item.d}</DataTable.Cell>
+                                <DataTable.Cell style={styles.title}>{item.i1}</DataTable.Cell>
+                                <DataTable.Cell style={styles.title}>{item.i2}</DataTable.Cell>
                             </DataTable.Row>
                         </DataTable>
                     </Card.Content>
                     <Card.Actions>
-                        <Button icon="pencil" mode="contained" style={{alignSelf:'center'}} onPress={() => console.log('Pressed')}>
+                        <Button icon="pencil" mode="contained" style={{ margin: 5 }} onPress={() => {
+                            setModalVisible(true);
+                            setCurrentIndex(index);
+                        }}>
                             Input
                         </Button>
                     </Card.Actions>
                 </Card>
             ))}
+            <View style={styles.formContainer}>
+                <DatePickerInput
+                    locale='en'
+                    label='Date'
+                    style={{ marginBottom: 10 }}
+                    value={date}
+                    onChange={setDate}
+                    inputMode='start'
+                    mode='outlined'
+                />
+                <TextInput
+                    style={{ marginBottom: 20 }}
+                    mode='outlined'
+                    label='Module'
+                    onChangeText={setModule}
+                    inputMode='numeric'
+                    value={module} />
+                <Button
+                    style={{ marginBottom: 40 }}
+                    mode="contained"
+                    onPress={handleInputScore}
+                    disabled={btnLoading || isLoading}
+                    loading={btnLoading}>
+                    Submit
+                </Button>
+                <ActivityIndicator animating={isLoading} />
+                {/* <Menu
+                    visible={menuVisible}
+                    onDismiss={() => setMenuVisible(false)}
+                    style={{ width: '80%' }}
+                    anchor={<Divider />}>
+
+                    <Menu.Item onPress={() => { }} title="Item 1" />
+                    <Menu.Item onPress={() => { }} title="Item 2" />
+                    <Menu.Item onPress={() => { }} title="Item 3" />
+                </Menu> */}
+            </View>
         </ScrollView>
     )
 };
@@ -110,40 +213,23 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    contentContainer: {
-        flex: 1,
-        justifyContent: 'center',
+    modalContainer: {
+        backgroundColor: 'white',
+        padding: 20,
+        paddingTop: 30,
+        paddingBottom: 30
+    },
+    formContainer: {
+        width: '80%',
+        alignSelf: 'center',
         marginTop: 15,
     },
     title: {
         justifyContent: 'center',
     },
-    input: {
-        width: '80%',
-        alignSelf: 'center'
-    },
-    button: {
-        marginTop: 20,
-        width: '60%',
-        alignSelf: 'center'
-    },
-    buttonText: {
-        fontSize: 20
-    },
-    // title: {
-    //     width: '80%',
-    //     marginBottom: 18,
-    //     fontWeight: 'bold'
-    // },
     subTitle: {
         width: '80%',
         marginBottom: 10,
+        marginTop: 10,
     },
-    welcome: {
-        color: 'white',
-        fontSize: 22,
-        fontWeight: 'bold',
-        marginBottom: 30,
-        textAlign: 'center'
-    }
 })
